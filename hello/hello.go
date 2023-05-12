455db4257a44c1687604c56e6d4569222568a776
Gani Mendoza <gani.mendoza@gmail.com>
2023-05-12 16:40:59 +0800

    test crdb

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,10 @@ package hello
 
 import (
 	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v4"
 )
 
 // Welcome to Encore!
@@ -18,7 +22,21 @@ import (
 //
 //encore:api public path=/hello/:name
 func World(ctx context.Context, name string) (*Response, error) {
-	msg := "Hello, " + name + "!"
+	connstring := os.Getenv("connstring")
+
+	// Attempt to connect
+	config, err := pgx.ParseConfig(os.ExpandEnv(connstring))
+	if err != nil {
+		log.Fatal("error configuring the database: ", err)
+	}
+	conn, err := pgx.ConnectConfig(context.Background(), config)
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	defer conn.Close(context.Background())
+	log.Println("Hey! You successfully connected to your CockroachDB cluster.")
+
+	msg := "Hello, " + name + ". You successfully connected to your CockroachDB cluster!"
 	return &Response{Message: msg}, nil
 }
 
